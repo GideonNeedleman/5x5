b509264e65096306325b4c85c8390bfab701b4d1
Gideon Needleman <gideon.works@zohomail.com>
2023-11-04 14:21:54 -0700

    ButtonGroups to add settings gear button

--- src/pages/SelectWorkout.jsx
+++ src/pages/SelectWorkout.jsx
@@ -1,6 +1,7 @@
 import Button from "react-bootstrap/Button";
 import Stack from "react-bootstrap/Stack";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
+import { BsGear } from "react-icons/bs";
 /* import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col"; */
@@ -11,10 +12,24 @@ function SelectWorkout() {
       <h1 className="d-flex justify-content-center">Select Workout</h1>
       <Stack gap={2} className="col-sm-5 mx-auto px-2">
         <ButtonGroup>
-          <Button variant="secondary">A day</Button>
-          <Button variant="secondary"></Button>
+          {/* <Button variant="secondary"></Button> */}
+          <Button className="w-100" variant="secondary">
+            A day
+          </Button>
+          <Button variant="secondary">
+            <BsGear />
+          </Button>
         </ButtonGroup>
-        <Button variant="secondary">B day</Button>
+
+        <ButtonGroup>
+          <Button className="w-100" variant="secondary">
+            B day
+          </Button>
+          <Button variant="secondary">
+            <BsGear />
+          </Button>
+        </ButtonGroup>
+
         <Button variant="outline-secondary">+ Add Workout</Button>
       </Stack>
     </main>
