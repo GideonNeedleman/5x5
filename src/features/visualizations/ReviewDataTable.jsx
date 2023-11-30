1ad4b3084ee8fbb13c3ea28496d125d563671a51
Gideon Needleman <gideon.works@zohomail.com>
2023-11-30 13:06:27 -0800

    Correctly filtering Review data for past 6 hours

--- src/features/visualizations/ReviewDataTable.jsx
+++ src/features/visualizations/ReviewDataTable.jsx
@@ -1,13 +1,20 @@
 import Table from "react-bootstrap/Table";
 import DataRow from "./DataRow";
 import { useGlobalContext } from "../../context/GlobalContext";
+import dayjs from "dayjs";
 
 // data is an array of set records: exerciseId,(exerciseType,) exerciseName, datetime, metrics, note
 
+const now = dayjs(new Date());
+const HOURS_TO_INCLUDE = 6;
+
 function ReviewDataTable({ exercise }) {
   const { workoutData } = useGlobalContext();
+  // Only include data that matches exercise and was logged within certain number of hours ago
   const exerciseData = workoutData.filter(
-    (set) => set.exerciseName === exercise.name
+    (set) =>
+      set.exerciseName === exercise.name &&
+      now.diff(dayjs(set.datetime), "hour") < HOURS_TO_INCLUDE
   );
 
   return (
