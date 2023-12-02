6a5b13b2afd216eee46270fc671d5540ec410a0a
Gideon Needleman <gideon.works@zohomail.com>
2023-12-01 22:33:59 -0800

    EditSetModal programmatically find input type

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -218,7 +218,7 @@ const initialState = {
       ],
     },
     {
-      name: "Some other program",
+      name: "Sample training program",
       id: 2,
       workouts: [
         {
--- src/features/visualizations/EditSetModal.jsx
+++ src/features/visualizations/EditSetModal.jsx
@@ -2,8 +2,22 @@ import { Stack } from "react-bootstrap";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
+import { useGlobalContext } from "../../context/GlobalContext";
 
 function EditSetModal({ show, onHide, set }) {
+  const { programData } = useGlobalContext();
+
+  // super nested search. First part digs to find the matching program, then again to find matching workout, then again to find matching exercise.
+  const exerciseMatch = programData
+    .find((program) =>
+      program.workouts.find((workout) =>
+        workout.exercises.find((exercise) => exercise.id === set.exerciseId)
+      )
+    )
+    .workouts.find((workout) =>
+      workout.exercises.find((exercise) => exercise.id === set.exerciseId)
+    )
+    .exercises.find((exercise) => exercise.id === set.exerciseId);
   return (
     <Modal
       size="lg"
@@ -19,7 +33,14 @@ function EditSetModal({ show, onHide, set }) {
           {set.metrics.map((metric) => (
             <label key={metric.name}>
               {metric.name}
-              <input defaultValue={metric.value} type="number" />
+              <input
+                defaultValue={metric.value}
+                type={
+                  exerciseMatch.metrics.find(
+                    (exMetric) => exMetric.name === metric.name
+                  ).type
+                }
+              />
             </label>
           ))}
           <label>
