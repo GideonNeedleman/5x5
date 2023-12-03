28fc5d07fa15bddda0765dad1308bfc2851f7302
Gideon Needleman <gideon.works@zohomail.com>
2023-12-03 00:48:28 -0800

    Fix - sign  on NumberIncrementBar fallback step

--- src/features/input-bars/NumberIncrementBar.jsx
+++ src/features/input-bars/NumberIncrementBar.jsx
@@ -23,7 +23,7 @@ function NumberIncrementBar({
         <Button
           variant="secondary"
           disabled={isFinished && !isUnlocked}
-          onClick={() => stepMetric(metric.name, -metric.inputBar?.step || 1)}
+          onClick={() => stepMetric(metric.name, -metric.inputBar?.step || -1)}
         >
           <BsDashLg />
         </Button>
