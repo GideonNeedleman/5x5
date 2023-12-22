784cdf618ab71c5728cb767dbd42b0d9148fefa2
Gideon Needleman <gideon.works@zohomail.com>
2023-12-22 00:27:25 -0800

    Add go home button to Review

--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -1,6 +1,9 @@
+import { Button } from "react-bootstrap";
 import { useGlobalContext } from "../context/GlobalContext";
 import ReviewDataTable from "../features/visualizations/ReviewDataTable";
 import WorkoutOverview from "../features/visualizations/WorkoutOverview";
+import vibrator from "vibrator";
+import { useNavigate } from "react-router-dom";
 
 function Review() {
   const { tempRecordData, exerciseData } = useGlobalContext();
@@ -10,11 +13,22 @@ function Review() {
   const exercises = exerciseData.filter((exercise) =>
     exerciseIds.includes(exercise.id)
   );
+  const navigate = useNavigate();
 
   return (
     <div>
       <h1 className="text-center display-3">Workout Summary</h1>
       <WorkoutOverview />
+      <Button
+        className="ms-2"
+        variant="warning"
+        onClick={() => {
+          vibrator(1);
+          navigate("/");
+        }}
+      >
+        Home
+      </Button>
       {exercises.map((exercise) => (
         <ReviewDataTable exercise={exercise} key={exercise.id} />
       ))}
