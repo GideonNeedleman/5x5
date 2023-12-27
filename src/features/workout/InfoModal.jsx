8c16eeb7229894efae1f0c3281a5139ae97e3c6d
Gideon Needleman <gideon.works@zohomail.com>
2023-12-26 22:43:00 -0800

    format InfoModal

--- src/features/workout/InfoModal.jsx
+++ src/features/workout/InfoModal.jsx
@@ -1,3 +1,4 @@
+import { Card, ListGroup } from "react-bootstrap";
 import Modal from "react-bootstrap/Modal";
 
 export default function InfoModal({ show, onHide, exercise }) {
@@ -10,32 +11,35 @@ export default function InfoModal({ show, onHide, exercise }) {
       centered
       onHide={onHide}
     >
-      <Modal.Title className="text-center mt-3" as={"h3"}>
-        Exercise Info
-      </Modal.Title>
-      <Modal.Body>
-        {hasDescription
-          ? exercise.description
-          : "Sorry, no exercise description available"}
-        <h3 className="text-center mt-2">Metric Info</h3>
-        <ul className="list-group list-group-flush">
-          {exercise.metrics.map((metric, index) => (
-            <li key={index} className="list-group-item">
-              <span className="text-capitalize">
-                {metric.name}:
-                {metric.adaptive ? (
-                  <span> adaptive</span>
-                ) : (
-                  <span> not adaptive</span>
-                )}
-              </span>
-            </li>
-          ))}
-        </ul>
-      </Modal.Body>
-      {/*       <Modal.Footer>
-        <Button onClick={onHide}>Close</Button>
-      </Modal.Footer> */}
+      <Card border="primary">
+        <Card.Header className="text-center" as={"h3"}>
+          Exercise Info
+        </Card.Header>
+        <Card.Body>
+          {hasDescription
+            ? exercise.description
+            : "Sorry, no exercise description available"}
+        </Card.Body>
+        <Card.Header className="text-center mt-2" as="h3">
+          Metric Info
+        </Card.Header>
+        <Card.Body className="p-0">
+          <ListGroup variant="flush" className="">
+            {exercise.metrics.map((metric, index) => (
+              <ListGroup.Item key={index} className="">
+                <span className="text-capitalize">
+                  {metric.name}:
+                  {metric.adaptive ? (
+                    <span> adaptive</span>
+                  ) : (
+                    <span> not adaptive</span>
+                  )}
+                </span>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        </Card.Body>
+      </Card>
     </Modal>
   );
 }
